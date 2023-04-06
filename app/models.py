7108d2aacf4d9fedd3a1465256d3c6a609a523ad
vboxuser <homyak72088@gmail.com>
2023-04-06 17:47:21 +0300

    dz 3.0 in porogress, added SQL

--- app/models.py
+++ app/models.py
@@ -4,3 +4,5 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, index=True)
+    email = db.Column(db.String, unique=True, index=True)
+    password = db.Column(db.String, unique=True, index=True)
