71d2eb0dcb51bd0087f31f00a9db50e6df109ee5
vboxuser <homyak72088@gmail.com>
2023-06-16 14:00:05 +0300

    fix

--- app/user/__init__.py
+++ app/user/__init__.py
@@ -9,19 +9,19 @@ bp = Blueprint('user', __name__, url_prefix='/user')
 from . import routes
 
 
-@bp.cli.command('extract_users')
-def extract_users():
-    users = User.query.all()
+#@bp.cli.command('extract_users')
+#def extract_users():
+#    users = User.query.all()
 
-    with open('users.csv', mode='w', newline='') as file:
-        fieldnames = ['username', 'email', 'full_name', 'post_count']
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
+#    with open('users.csv', mode='w', newline='') as file:
+#        fieldnames = ['username', 'email', 'full_name', 'post_count']
+#        writer = csv.DictWriter(file, fieldnames=fieldnames)
 
-        writer.writeheader()
-        for user in users:
-            writer.writerow({
-                'username': user.username,
-                'email': user.email,
-                'full_name': user.full_name,
-                'post_count': len(user.posts)
-            })
+#        writer.writeheader()
+#       for user in users:
+#            writer.writerow({
+#                'username': user.username,
+#                'email': user.email,
+#               'full_name': user.full_name,
+#                'post_count': len(user.posts)
+#           })
